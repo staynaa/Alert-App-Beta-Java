f76550f5958df20f713a5ccdb1d01a86bf172694
Stayna <staynaa.cs@gmail.com>
2023-11-04 19:20:18 -0400

    made an add contact function

--- AlertApp/src/App.java
+++ AlertApp/src/App.java
@@ -15,16 +15,30 @@ public class App extends UserAction{
             System.out.println("Error: unable to load driver class!");
             System.exit(1);
         }
+        
         UserAction curruser;
+        String uaopt;
         System.out.println("Welcome To Alert App\nSelect Option\nA. Sign Up\nB. Log In\nAny Key To Exit App.");
         switch(input.next()){
             case("A"):
             curruser=signUp(input);
             curruser.showMenu(); 
+            uaopt=option(input);
+            while(!uaopt.equals("E")){
+                choice(curruser,uaopt);
+                curruser.showMenu(); 
+                uaopt=option(input);
+            }
             break;
             case("B"):
             curruser=logIn(input);
-            curruser.showMenu();
+            curruser.showMenu(); 
+            uaopt=option(input);
+            while(!uaopt.equals("E")){
+                choice(curruser,uaopt);
+                curruser.showMenu(); 
+                uaopt=option(input);
+            }
             break;
             default:
             System.out.println("App Exited. Good Bye");
@@ -35,7 +49,8 @@ public class App extends UserAction{
         input.close();
     }
     public static UserAction signUp(Scanner input){
-        String ufn, uln, uem, upn, SQL; // user info 
+        String ufn, uln, uem, upn, SQL, SQL2; // user info 
+        int usID;
         try (Connection con = DriverManager.getConnection(url,user,password)) { //create connection with databse
             System.out.println("Sign Up Window\nEnter information for following fields\nFirst Name: ");
             ufn = input.next();
@@ -45,17 +60,29 @@ public class App extends UserAction{
             uem = input.next();
             System.out.println("Phone Number: ");
             upn = input.next();
-            System.out.println("Welcome To AlertMe");
+            // System.out.println("Welcome To AlertMe");
 
             Statement stmt = con.createStatement();
             SQL = "INSERT INTO userinfo VALUES (NULL, '" + ufn + "', '" + uln + "','" + uem + "', '+1" + upn + "')"; //query to insert new account
-            stmt.executeUpdate(SQL); //executes the query
-            UserAction ua= new UserAction(ufn, uln);
-            System.out.println("Registration Complete!");
-            return ua;
+            stmt.executeUpdate(SQL); //executes the query insert
+            //System.out.println("------------>first query done ");
+            SQL2 = "SELECT `usid` FROM `userinfo` WHERE `email`=?"; //query to get id
+            
+            PreparedStatement stmt2 = con.prepareStatement(SQL2);
+            stmt2.setString(1, uem);
+            ResultSet rs= stmt2.executeQuery(); //executes the query
+            // DEBUG System.out.println("QUERY WORKED");
+            //System.out.println("------------>second query done ");
+            while(rs.next()){
+                //System.out.println("IN WHILE LOOP");
+                usID= rs.getInt("usid");
+                UserAction ua= new UserAction(ufn, uln, con, usID);
+                System.out.println("Registration Complete!");
+                return ua;
+            }
         } catch (SQLException e) {
             // e.printStackTrace();
-            System.out.println("SQL EXCEPTION");
+            System.out.println("SQL EXCEPTION in sign up");
         }
         //if the first return didnt work
         UserAction invaliduser =new UserAction();
@@ -63,13 +90,14 @@ public class App extends UserAction{
     }
     public static UserAction logIn(Scanner input){
         String ufn, uln, uem, upn, SQL;
+        int usID;
         try (Connection con = DriverManager.getConnection(url,user,password)) { //create connection with databse
             System.out.println("Log In Window\nEnter your information for following fields\nEmail: ");
             uem= input.next();
             System.out.println("Phone Number: ");
             upn= input.next();
             // SQL = "SELECT fname, lname FROM userinfo WHERE email= ? AND userphone= ?"; //query to find existing account and log in
-            SQL = "SELECT `fname`, `lname` FROM `userinfo` WHERE `email`= ? AND `userphone`= ?";
+            SQL = "SELECT `fname`, `lname`, `usid` FROM `userinfo` WHERE `email`= ? AND `userphone`= ?";
             PreparedStatement stmt = con.prepareStatement(SQL);
             stmt.setString(1, uem);
             stmt.setString(2, "+1"+upn);
@@ -80,16 +108,64 @@ public class App extends UserAction{
                 // DEBUG System.out.println("IN WHILE LOOP");
                 ufn = rs.getString("fname");
                 uln = rs.getString("lname");
-                UserAction ua= new UserAction(ufn,uln);
+                usID= rs.getInt("usid");
+                UserAction ua= new UserAction(ufn,uln, con, usID);
                 System.out.println("Log In Complete!");
                 return ua;
             }
         } catch (SQLException e) {
             // e.printStackTrace();
-            System.out.println("SQL EXCEPTION");
+            System.out.println("SQL EXCEPTION in log in");
         }
         //if the first return didnt work
         UserAction invaliduser =new UserAction();
         return invaliduser;
+    }
+    public static String option(Scanner input){
+        String opt= input.next();
+        return opt;
+    }
+    public static void choice(UserAction ua, String opt){
+        switch(opt){
+            case("A"):
+                addEmergencyContact(ua.userID);
+                break;
+            case("B"):
+                ua.quickAlert();
+                break;
+            case ("C"):
+                ua.alertContact();
+                break;
+        }
+    }
+
+    public static void addEmergencyContact(int usid) {
+        System.out.println("checking if id works......id:"+usid);
+        Scanner in= new Scanner(System.in);
+        String cfname,clname,contphone, SQL;
+        System.out.println("This is where you add an emergency contact");
+        try(Connection con = DriverManager.getConnection(url,user,password)){
+            System.out.println("Enter information for following fields\nContact's First Name: ");
+            cfname = in.next();
+            System.out.println("Contact's Last Name: ");
+            clname = in.next();
+            System.out.println("Contact's Phone Number: ");
+            contphone = in.next();
+            System.out.println("Registering Emergency Contact.....");
+            //in.close();
+            SQL= "INSERT INTO `emergencycont`(cid,usid,cfname,clname,contphone,active) VALUES(NULL,?,?,?,?,1)";
+            PreparedStatement pstmt = con.prepareStatement(SQL);
+            System.out.println("DEBUG 1");
+            //SQL = "INSERT INTO `emergencycont` VALUES (NULL, "+usid+",'" + cfname + "', '" + clname + "','" + "'+1" + contphone + "', 1)"; //query to insert new contact
+            pstmt.setInt(1, usid);
+            pstmt.setString(2,cfname );
+            pstmt.setString(3,clname );
+            pstmt.setString(4,contphone );
+            System.out.println("DEBUG 2");
+            pstmt.executeUpdate(); //executes the query
+            System.out.println("DEBUG 3");
+        } catch(SQLException e){
+            System.out.println("SQL EXCEPTION in add emergency");
+        }
     }
 }
--- AlertApp/src/UserAction.java
+++ AlertApp/src/UserAction.java
@@ -1,5 +1,9 @@
+import java.sql.*;
+//import java.util.Scanner;
 public class UserAction {
     String fname, lname;
+    int userID;
+    Connection uacon;
     boolean valid;
 
     UserAction() {
@@ -7,30 +11,50 @@ public class UserAction {
         this.valid = false;
     }
 
-    UserAction(String fname, String lname) {
+    UserAction(String fname, String lname, Connection con, int usid) {
         this.fname = fname;
         this.lname = lname;
+        this.uacon=con;
+        this.userID=usid;
         this.valid = true;
+        System.out.println("USER ID:"+userID);
     }
 
     public void showMenu() {
         if (valid) {
-            System.out.println("Welcome to Alert App " + fname + " " + lname
-                    + "!\nWhat would you like to do? Select an option:" +
-                    "\nA. Add an emergency contact\nB. Send A Quick Alert\nC.Send An Alert With A Message\nE. Exit App");
+            System.out.println("\n|------------------------|\nWelcome to Alert App " + fname + " " + lname
+            + "!\nWhat would you like to do? Select an option:\nA. Add an emergency contact\nB."+
+            " Send A Quick Alert\nC.Send An Alert With A Message\nE. Exit App\n|------------------------|\n");
         }
         else System.exit(-1);
     }
 
-    public void addEmergencyContact() {
-
-    }
+    // public void addEmergencyContact() {
+    //     Scanner input= new Scanner(System.in);
+    //     String cfname,clname,contphone, SQL;
+    //     System.out.println("This is where you add an emergency contact");
+    //     try{
+    //         System.out.println("Enter information for following fields\nContact's First Name: ");
+    //         cfname = input.next();
+    //         System.out.println("Contact's Last Name: ");
+    //         clname = input.next();
+    //         System.out.println("Contact's Phone Number: ");
+    //         contphone = input.next();
+    //         System.out.println("Registering Emergency Contact.....");
+    //         input.close();
+    //         Statement stmt = uacon.createStatement();
+    //         SQL = "INSERT INTO emergencycont VALUES (NULL, "+this.userID+",'" + cfname + "', '" + clname + "','" + "'+1" + contphone + "', 1)"; //query to insert new contact
+    //         stmt.executeUpdate(SQL); //executes the query
+    //     } catch(SQLException e){
+    //         System.out.println("SQL EXCEPTION in add emergency");
+    //     }
+    // }
 
     public void alertContact() {
-        System.out.println("Enter Text: ");
+        System.out.println("Type your Alert Message\nEnter Text: ");
         
     }
     public void quickAlert(){
-        System.out.println("");
+        System.out.println("We will send a quick alert");
     }
 }
